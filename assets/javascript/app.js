c208b2153977d455d2dafa720eea32faa20cbfe7
Anna Ly <annatly2@gmail.com>
2017-08-30 22:51:38 -0700

    aug 30

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,14 +1,46 @@
 $(document).ready(function(){
 
 var firstScreen;
-var counter = 10;
+var counter = 5;
 var intervalId;
 
-var unansweredQuestion;
+
 var triviaHTML;
-var numAnswerCorrect;
-var numAnswerWrong;
+var numAnswerCorrect = 0;
+var numAnswerWrong = 0;
+var unansweredQuestion = 0;
 var questionCounter = 0;
+var userAnswer;
+
+
+var questions =[{
+	question: "In The Little Mermaid, when Ursula transformed herself into a bride for Price Eric, what did she call herself?",
+	choice: ["Veronica", "Vanessa", "Victoria", "Vivian"],
+	correctAnswer: 1
+},{
+	question: "At the end of The Emperor's New Groove, what animal does Yzma turn into?",
+	choice: ["Cat", "Dog", "Cow", "Llama"],
+	correctAnswer: 1
+}, {
+	question: "What is Maleficent's crow called?", 
+	choice: ["Raven", "Lucifer", "Diablo", "Rook"],
+	correctAnswer: 2
+}];
+
+var currentQuestion = 0;
+var correctAnswers = 0;
+var quizOver = false;
+
+function displayCurrentQuestion(){
+
+
+$("#question").html("<h2>"+questions[0].question+"</h2>");
+$("#choices").html("<h2>"+questions[0].choice+"</h2>");
+
+
+}
+
+displayCurrentQuestion();
 
 
 function startScreen(){
@@ -19,16 +51,18 @@ function startScreen(){
 startScreen();
 
 $("body").on("click", ".start-button", function (event){
-	//insert functions to generate HTML and start timer
+	generateHTML();
+	runTimer();
 });
 
 function generateHTML(){
-
+	triviaHTML= "<p class='text-center timer-p'>Time Remaining: <span class='timer'>10</span></p><p class='text-center'>" + questions[questionCounter].question + "</p><p class='first-answer answer'>A. " + questions[questionCounter].choice[0] + "</p><p class='answer'>B. "+questions[questionCounter].choice[1]+"</p><p class='answer'>C. "+questions[questionCounter].choice[2]+"</p><p class='answer'>D. "+questions[questionCounter].choice[3]+"</p>";
+	$(".mainArea").html(triviaHTML);
 }
 
 function lossDueToTimeOut(){
 	unansweredQuestion++;
-	triviaHTML = "<p class='text-center timer-p'>Time Remaining: <span class='timer'>" + counter + "</span></p>" + "<p class='text-center'>You ran out of time!  The correct answer was: " + correctAnswers[questionCounter] + "</p>" + "<img class='center-block img-wrong' src='img/x.png'>";
+	triviaHTML = "<p class='text-center timer-p'>Time Remaining: <span class='timer'>" + counter + "</span></p>" + "<p class='text-center'>You ran out of time!  The correct answer was: " + questions[questionCounter].choice[questions[questionCounter].correctAnswer] + "</p>" + "<img class='center-block img-wrong' src='https://68.media.tumblr.com/ea27c6140ece2a7be399c753d7d1f00b/tumblr_inline_nfq90upz0i1t2hh7q.gif'>";
 	$(".mainArea").html(triviaHTML);
 	setTimeout(wait, 3000);
 
@@ -92,40 +126,7 @@ function finalScreen(){
 
 
 
-//Questions
 
-var questions =[{
-	question: "In The Little Mermaid, when Ursula transformed herself into a bride for Price Eric, what did she call herself?",
-	choice: ["Veronica", "Vanessa", "Victoria", "Vivian"],
-	correctAnswer: 1
-},{
-	question: "At the end of The Emperor's New Groove, what animal does Yzma turn into?",
-	choice: ["Cat", "Dog", "Cow", "Llama"],
-	correctAnswer: 1
-}, {
-	question: "What is Maleficent's crow called?", 
-	choice: ["Raven", "Lucifer", "Diablo", "Rook"],
-	correctAnswer: 2
-}];
-
-var currentQuestion = 0;
-var correctAnswers = 0;
-var quizOver = false;
-
-
-
-
-function displayCurrentQuestion(){
-
-
-$("#question").html("<h2>"+questions[0].question+"</h2>");
-$("#choices").html("<h2>"+questions[0].choice+"</h2>");
-
-
-}
-
-
-displayCurrentQuestion();
 
 });
 
@@ -139,4 +140,4 @@ displayCurrentQuestion();
 //https://wereallmadinhere.files.wordpress.com/2012/10/260wtv7-jpg.gif
 
 ///ursula picture
-//https://68.media.tumblr.com/ea27c6140ece2a7be399c753d7d1f00b/tumblr_inline_nfq90upz0i1t2hh7q.gif
+//
